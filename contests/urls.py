82b7ab58fd9d927b3a5a70f0ad4012845c0b1e91
Saket Patel <saket.is.sam@gmail.com>
2017-06-28 23:42:03 +0530

    Urls and views to view and edit contest and problems

--- contests/urls.py
+++ contests/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
 	url(r'^$', views.index, name="index"),
 	url(r'^create/$', views.create, name="create"),
 	url(r'^q/(?P<code>[a-zA-z0-9]{4,15})/$', views.contest, name="contest"),
+	url(r'^q/edit/(?P<code>[a-zA-z0-9]{4,15})/$', views.editc, name="editc"),
+	url(r'^q/edit/(?P<code>[a-zA-z0-9]{4,15})/(?P<question>[a-zA-Z0-9]){4, 15}/$', views.editq, name="editq"),
+	url(r'^q/(?P<code>[a-zA-z0-9]{4,4})/p/(?P<question>[a-zA-Z0-9]{3,15})/', views.problem, name="problem"),
+	url(r'^q/edit/(?P<code>[a-zA-z0-9]{4,15})/a/$', views.addq, name="problem_add"),
+
 ]
--- contests/views.py
+++ contests/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Contest, Problem
@@ -7,6 +7,7 @@ from django.db.models import Q
 from .forms import *
 from django.contrib.auth.models import User
 # Create your views here.
+import pytz
 
 
 @login_required(login_url='/')
@@ -64,5 +65,63 @@ def create(request):
 #	return HttpResponse("Welcome, here you can create your own contests!")
 
 @login_required(login_url="/")
-def contest(request, code=None):
-	return HttpResponse("Ok!!")
+def contest(request, code):
+	print(code)
+	con = get_object_or_404(Contest, Q(contest_code=code))
+#	if(con.)
+	dt = datetime.datetime.now().replace(tzinfo=pytz.timezone('Asia/Kolkata'))-con.start_date.replace(tzinfo=pytz.timezone('Asia/Kolkata'))
+	if(dt.days < 0 or dt.hours < 0 or dt.seconds < 0):
+		pp = True
+	else:
+		pp = False
+	return render(request, 'contests/contest.html', {'contest':con, 'pp':pp})
+#	return HttpResponse("HoHO")
+
+@login_required(login_url="/")
+def editc(request, code):
+	con = get_object_or_404(Contest, Q(contest_code=code)&Q(end_date__gte=datetime.datetime.now()))
+#	print con.admin
+#	print request.user
+	if(con.admin == request.user):
+		return render(request, 'contests/editc.html', {'contest':con})
+	return render(request, 'contests/editc.html', {'contest':False})
+	#	return HttpResponse("Well, going to add it, soon")
+
+
+@login_required(login_url="/")
+def editq(request, code, question):
+	if(request.method=="POST"):
+		pass
+	return HttpResponse("Well, going to add it, soon")
+
+
+@login_required(login_url="/")
+def problem(request, code, question):
+	con = get_object_or_404(Contest, contest_code=code)
+	ques = get_object_or_404(Problem, code=question)
+	return render(request, 'contests/problem.html', {'contest':con, 'ques':ques})
+
+
+
+@login_required(login_url="/")
+def addq(request, code):
+	if(request.method=="POST"):
+		try:
+			con = Contest.objects.get(contest_code=code)
+			form = Prob(request.POST)
+			if form.is_valid():
+				pb = Problem.objects.create(
+					setter = request.user,
+					contest = con,
+					name = form.cleaned_data['name'],
+					code = form.cleaned_data['code'],
+					text = form.cleaned_data['text'],
+					time_lim = form.cleaned_data['time_lim']
+				)
+				pb.save()
+			else:
+				return render(request, 'contests/addq.html', {'form':Prob(initial={})})
+		except Exception as e:
+			print(str(e))
+			return render(request, 'contests/addq.html', {'form':Prob(initial={})})
+	return render(request, 'contests/addq.html', {'form':Prob(initial={})})
