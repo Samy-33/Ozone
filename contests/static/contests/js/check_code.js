f929e33e227cf04077e2927ae48277de657afd26
Saket Patel <saket.is.sam@gmail.com>
2017-07-01 01:27:47 +0530

    submission for a particular problem

---
+++ contests/static/contests/js/check_code.js
@@ -0,0 +1,47 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    }
+});
+
+
+
+function submit(problem, token){
+	var code = document.getElementById("editcode").value;
+//	alert(code);
+	alert(token);
+	var lang = document.getElementById("lang").value;
+
+	$.ajax({
+		type:'POST',
+		url: '/contests/q/submit/'+problem+"/",
+		data:{'code':code, 'lang':lang},
+		beforeSend: function(xhr, settings){
+			console.log("Before send");
+			$.ajaxSettings.beforeSend(xhr, settings);
+		},
+		success:function(data){
+			document.getElementById("result").innerHTML = data;
+			console.log(data)
+		}
+	});
+}
